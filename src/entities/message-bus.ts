0da5381083850380c0fb8e7de55f2906911ef2a5
Valentina <64658886+valebearzotti@users.noreply.github.com>
2025-05-02 15:25:06 -0300

    Update MessageBus to add queueable events support

--- src/entities/message-bus.ts
+++ src/entities/message-bus.ts
@@ -1,14 +1,18 @@
 import { IMessageBus, Message, MessageHandler } from "../models/message";
+import { ProcessorOptions, QueueOptions, QueuedEvent, QueueStatus } from "../models/queue";
 import { Subscription } from "../models/subscription";
 import { AvailableTopic, TopicMap } from "../models/topic";
 import { generateMessageId } from "../utils/id";
 import { EventRegistry } from "./registry";
+import { QueueProcessor } from "./queue-processor";
 
 export class MessageBus<TMap extends TopicMap> implements IMessageBus<TMap> {
   private registry: EventRegistry;
+  private queueProcessor: QueueProcessor<TMap>;
 
   constructor() {
     this.registry = new EventRegistry();
+    this.queueProcessor = new QueueProcessor<TMap>(this.registry);
   }
 
   /**
@@ -41,6 +45,60 @@ export class MessageBus<TMap extends TopicMap> implements IMessageBus<TMap> {
     await Promise.all(handlerPromises);
   }
 
+  /**
+   * Queue an event for later processing
+   */
+  queueEvent<Topic extends AvailableTopic<TMap>>(
+    topic: Topic,
+    payload: TMap[Topic],
+    options?: QueueOptions
+  ): QueuedEvent<TMap[Topic]> {
+    const message: Message<TMap[Topic]> & { topic: string } = {
+      id: generateMessageId(topic),
+      payload,
+      timestamp: Date.now(),
+      topic: topic as string
+    };
+    
+    return this.queueProcessor.queueEvent(topic as string, message);
+  }
+
+  /**
+   * Start processing queued events
+   */
+  startQueueProcessor(options?: ProcessorOptions): void {
+    if (options) {
+      // Create a new processor with the specified options
+      this.queueProcessor = new QueueProcessor<TMap>(this.registry, options);
+    }
+    
+    this.queueProcessor.start();
+  }
+
+  /**
+   * Stop processing queued events
+   */
+  stopQueueProcessor(): void {
+    this.queueProcessor.stop();
+  }
+
+  /**
+   * Process all queued events immediately
+   */
+  async processQueue(): Promise<void> {
+    await this.queueProcessor.processAll();
+  }
+
+  /**
+   * Get current queue status
+   */
+  getQueueStatus(): QueueStatus {
+    return this.queueProcessor.getStatus();
+  }
+
+  /**
+   * Subscribe to events of a specific topic
+   */
   subscribe<Topic extends AvailableTopic<TMap>>(
     topic: Topic,
     handler: MessageHandler<TMap[Topic]>
